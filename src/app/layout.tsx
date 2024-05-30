e2fd677ebee69639e60fb110388ce74fe719b5f0
ejinn1 <ejinn1@naver.com>
2024-05-30 23:39:22 +0900

    chore: styled-compoent ssr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import StyledComponentsRegistry from "@/libs/registry";
 import StyledThemeProvider from "@/provider/ThemeProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <StyledThemeProvider>{children}</StyledThemeProvider>
+        <StyledComponentsRegistry>
+          <StyledThemeProvider>{children}</StyledThemeProvider>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
